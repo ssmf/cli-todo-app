0d027b01ee474b4becddfaca632b355e5f61d99e
ssmf <chamberkid1@gmail.com>
2023-10-30 20:21:30 +0100

    Added -help flag and some basic coloring to prints

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"os"
 
 	"example.com/packages/todo"
@@ -17,11 +18,17 @@ func main() {
 
 	AddTask := flag.Bool("add", false, "Specifies if user wants to add a new task")
 	RemoveTask := flag.Bool("rm", false, "Specifies if user wants to remove an existing task")
-	TaskName := flag.String("name", "BoilerplateTask", "Define a task name")
-	TaskDone := flag.Bool("done", false, "Define if task is done")
+	TaskName := flag.String("name", "BoilerplateTask", "Defines a task name")
+	TaskDone := flag.Bool("done", false, "Defines if task is done")
 	ShowList := flag.Bool("list", false, "Show current list of todos")
+	Help := flag.Bool("help", false, "Displays all commands and their roles")
 
 	flag.Parse()
+
+	if *Help && !*AddTask && !*RemoveTask && !*ShowList && !*TaskDone {
+		fmt.Println("it works!")
+		todo.DisplayHelp()
+	}
 
 	if *AddTask && *RemoveTask {
 		panic("You cannot remove and add tasks at the same time")
--- todo/todo.go
+++ todo/todo.go
@@ -6,6 +6,12 @@ import (
 	"os"
 )
 
+var Purple = "\033[35m"
+var Cyan = "\033[36m"
+var Red = "\033[31m"
+var Green = "\033[32m"
+var Reset = "\033[0m"
+
 type Task struct {
 	Name string
 	Done bool
@@ -44,17 +50,44 @@ func (t *Task) DoTask() {
 func (l *TaskList) DisplayList(b bool) {
 	if b {
 		for i, v := range *l {
-			fmt.Printf("[%d]: %s - %s \n", i+1, v.Name, IfDoneDisplay(v.Done))
+			fmt.Printf("\n[%d]: %s%s - %s \n\n%s", i+1, Purple, v.Name, IfDoneDisplay(v.Done), Reset)
 		}
 	}
+}
+
+func DisplayHelp() {
+	fmt.Printf("Here is a high level explenation of how this app works: ")
+	fmt.Printf("\n\nEvery part of this cli app is related to flags. For an example, if we want to:"+
+		" \n1.Add a new task"+
+		" \n2.Provide a name for it"+
+		"\nWe'll use 2 flags: -add as well as -name here are the flags applied:"+
+		"\n\n%smain.exe -add -name Read%s", Cyan, Reset)
+
+	fmt.Printf("\n\nHere is a list of all commands available:")
+	//All flags explained
+	fmt.Printf("\n\n")
+	for i := 0; i < 30; i++ {
+		fmt.Printf("-")
+	}
+	fmt.Printf("\n%s-add %s-> %sSpecifies if user wants to %sadd a new task%s", Green, Reset, Cyan, Green, Reset)
+	fmt.Printf("\n%s-rm %s-> %sSpecifies if user wants to %sremove an existing task%s", Green, Reset, Cyan, Red, Reset)
+	fmt.Printf("\n%s-name %s[value] %s-> %sDefines a task name%s", Green, Purple, Reset, Cyan, Reset)
+	fmt.Printf("\n%s-done %s-> %sDefines if task is done, if not used value is %sfalse%s", Green, Reset, Cyan, Red, Reset)
+	fmt.Printf("\n%s-list %s-> %sDisplays current list of todos%s", Green, Reset, Cyan, Reset)
+	fmt.Printf("\n%s-help %s-> %sDisplays all commands and their roles%s", Green, Reset, Cyan, Reset)
+	fmt.Printf("\n")
+	for i := 0; i < 30; i++ {
+		fmt.Printf("-")
+	}
+	fmt.Printf("\n")
 }
 
 func IfDoneDisplay(b bool) string {
 	var txt string
 	if b {
-		txt = "Done!"
+		txt = Green + "Done!"
 	} else {
-		txt = "Not done yet!"
+		txt = Red + "Not done yet!"
 	}
 	return txt
 }
